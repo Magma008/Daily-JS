b44b2c46b7993c57c41ea926c88d446c2bed67c1
Magma008 <unknownmagma008@gmail.com>
2026-01-11 10:30:16 +0500

    11 January 2026 Sunday 10:30AM

---
+++ To sqrt or not sqrt/app.js
@@ -0,0 +1,28 @@
+// ? var input = [4, 3, 9, 7, 2, 1];
+// ? var expected = [2, 9, 3, 49, 4, 1];
+// ? Test.assertSimilar(squareOrSquareRoot(input), expected);
+
+// ? input = [100, 101, 5, 5, 1, 1];
+// ? expected = [10, 10201, 25, 25, 1, 1];
+// ? Test.assertSimilar(squareOrSquareRoot(input), expected);
+
+// ? input = [1, 2, 3, 4, 5, 6];
+// ? expected = [1, 4, 9, 2, 25, 36];
+// ? Test.assertSimilar(squareOrSquareRoot(input), expected);
+
+// & Write a method, that will get an integer array as parameter and will process every number from this array.
+// & Return a new array with processing every number of the input-array like this:
+// & If the number has an integer square root, take this, otherwise square the number.
+
+// ! [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+
+let arr = [4,3,9,7,2,1];
+
+// let arr1 = ( arr.map((item) => item = Number.isInteger(Math.sqrt(item))) ? Math.sqrt(item) : Math.pow(item, 2) );
+// console.log(Number.isInteger(Math.sqrt(arr[0])));
+
+let arr2 = arr.map( (num) => num = ( Number.isInteger(Math.sqrt(num)) ? Math.sqrt(num) : Math.pow(num, 2) ) )
+
+console.log(arr2);
+
+const sqrtDecider = (nums) => 
