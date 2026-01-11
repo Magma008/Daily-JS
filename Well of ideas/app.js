cdc5c0f83622d373b93f9feae053d03c25d3d7e1
Magma008 <unknownmagma008@gmail.com>
2026-01-11 09:08:02 +0500

    11 January 2026 Sunday 09:08AM

--- Well of ideas/app.js
+++ Well of ideas/app.js
@@ -1,3 +1,16 @@
-assert.strictEqual(well(['bad', 'bad', 'bad']), 'Fail!');
-assert.strictEqual(well(['good', 'bad', 'bad', 'bad', 'bad']), 'Publish!');
-assert.strictEqual(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']), 'I smell a series!');
+// $ assert.strictEqual(well(['bad', 'bad', 'bad']), 'Fail!');
+// $ assert.strictEqual(well(['good', 'bad', 'bad', 'bad', 'bad']), 'Publish!');
+// $ assert.strictEqual(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']), 'I smell a series!');
+
+// % For every good kata idea there seem to be quite a few bad ones!
+
+// % In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+// # ['good', 'bad', 'bad', 'bad'] => "Publish"
+
+const goodCounter = (array) => {
+    let count = array.filter((item) => item == "good").length;
+    return ( count == 1 || count == 2 ? "Publish! 🖨️" : count == 0 ? "Fail! ⛔" : "I smell a series! 🔢" )
+};
+
+goodCounter(['good', 'bad', 'bad', 'bad', 'bad', "good"]);
