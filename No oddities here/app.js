3069173a9bb7f3a7b241f9543f423577bdd10600
Magma008 <unknownmagma008@gmail.com>
2026-01-11 11:45:27 +0500

    11 January 2026 Sunday 11:45AM

--- No oddities here/app.js
+++ No oddities here/app.js
@@ -4,3 +4,10 @@
 // & Write a small function that returns the values of an array that are not odd.
 // & All values in the array will be integers. Return the good values in the order they are given.
 
+// ! [0, 1, 2, 3, 4, 5, 6, 7] => [0, 2, 4, 6]
+
+const notOdd = (array) => {
+    return array.filter((number) => number % 2 === 0)
+};
+
+notOdd([12, 98, 1, 2, 34, 15]);
