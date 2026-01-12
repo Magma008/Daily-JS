68f7129d5367d181a3259d2ae90f13cc185823ee
Magma008 <unknownmagma008@gmail.com>
2026-01-12 11:22:55 +0500

    12 January 2026 Monday 11:22AM

---
+++ Pick peaks - Not Done/app.js
@@ -0,0 +1,22 @@
+// let arr = [3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]
+// let mpty = []
+// let peak;
+
+// // let mapped = arr.filter((num, index) => num > arr[index - 1] && num > arr[index + 1])
+
+// let mapped2 = arr.map((num, index) => {
+//     if (num > arr[index - 1] && num > arr[index + 1]) {
+//         mpty.push(num)
+//     } else {
+//         if (num > arr[index - 1]) {
+//             peak = num
+//         } else if (peak > arr[index + 1]) {
+//             mpty.push(peak)
+//         } else if (!peak > arr[index + 1]) {
+//             peak = "";
+//         }
+
+//     }
+// })
+
+// console.log(mpty);
