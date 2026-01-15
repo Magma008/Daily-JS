0fa9d8174d4c4e66b3c96b60d6f0520b75d7d226
Magma008 <unknownmagma008@gmail.com>
2026-01-15 10:22:25 +0500

    15 January 2026 Thursday 10:22AM

---
+++ Array helpers/app.js
@@ -0,0 +1,61 @@
+// # const numbers = [1, 2, 3, 4, 5];
+// # assert.deepEqual(numbers.square(), [1, 4, 9, 16, 25]);
+// # assert.deepEqual(numbers.cube(), [1, 8, 27, 64, 125]);
+// # assert.strictEqual(numbers.sum(), 15, 'Wrong sum');
+// # assert.strictEqual(numbers.average(), 3, 'Wrong average');
+// # assert.deepEqual(numbers.even(), [2, 4], 'Wrong result for even()');
+// # assert.deepEqual(numbers.odd(), [1, 3, 5], 'Wrong result for odd()');
+
+// & This kata is designed to test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in this class with the following methods: square(), cube(), average(), sum(), even() and odd().
+// & square() must return a copy of the this, containing all values squared
+// & cube() must return a copy of the this, containing all values cubed
+// & average() must return the average of all this values; on an empty this must return NaN (note: the empty this is not tested in Ruby!)
+// & sum() must return the sum of all this values
+// & even() must return an this of all even numbers
+// & odd() must return an this of all odd numbers
+// & Note: the original this must not be changed in any case!
+
+// ! var numbers = [1, 2, 3, 4, 5];
+// ! numbers.square();  =>  must return [1, 4, 9, 16, 25]
+// ! numbers.cube();    =>  must return [1, 8, 27, 64, 125]
+// ! numbers.average(); =>  must return 3
+// ! numbers.sum();     =>  must return 15
+// ! numbers.even();    =>  must return [2, 4]
+// ! numbers.odd();     =>  must return [1, 3, 5]
+
+Array.prototype.square = function () {
+    return this.map((num) => Math.pow(num, 2))
+}; 
+
+Array.prototype.cube = function () {
+    return this.map((num) => Math.pow(num, 3))
+};
+
+Array.prototype.average = function () {
+    return this.reduce((acc, val) => acc + val, 0) / this.length || NaN
+}; 
+
+Array.prototype.sum = function () {
+    return this.reduce((acc, val) => acc + val, 0)
+}; 
+
+Array.prototype.even = function () {
+    return this.filter((num) => num % 2 == 0)
+}; 
+
+Array.prototype.odd = function () {
+    return this.filter((num) => num % 2 != 0)
+}; 
+
+let arr = [1, 2, 3, 4, 5, 9];
+
+console.log(arr.square());
+console.log(arr.cube());
+console.log(arr.average());
+console.log(arr.sum());
+console.log(arr.even());
+console.log(arr.odd());
+
+
+
+
