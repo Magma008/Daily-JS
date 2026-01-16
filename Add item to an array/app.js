72a6297086d32f9d54f2762d125bf2a893026da6
Magma008 <unknownmagma008@gmail.com>
2026-01-16 10:30:30 +0500

    16 January 2026 Friday 10:30AM

--- Add item to an array/app.js
+++ Add item to an array/app.js
@@ -1,12 +1,12 @@
-// it("Should not be empty", () => {
-//     assert.isNotEmpty(websites, 'The array is still empty')
-// })
-// it('Should contain "codewars"', () => {
-//     assert.include(websites, 'codewars', 'The array does not contain the correct value "codewars"')
-// });
-// it("Should only contain 1 item", () => {
-//     assert.lengthOf(websites, 1)
-// })
+// $ it("Should not be empty", () => {
+// $     assert.isNotEmpty(websites, 'The array is still empty')
+// $ })
+// $ it('Should contain "codewars"', () => {
+// $     assert.include(websites, 'codewars', 'The array does not contain the correct value "codewars"')
+// $ });
+// $ it("Should only contain 1 item", () => {
+// $     assert.lengthOf(websites, 1)
+// $ })
 
 // * Add the value "codewars" to the websites array.
 // * After your code executes the websites array should == ["codewars"]
---
+++ The vowel code/app.js
@@ -0,0 +1,39 @@
+// ; assert.equal(encode('hello'), 'h2ll4');
+// ; assert.equal(encode('How are you today?'), 'H4w 1r2 y45 t4d1y?');
+// ; assert.equal(encode('This is an encoding test.'), 'Th3s 3s 1n 2nc4d3ng t2st.');
+// ; assert.equal(decode('h2ll4'), 'hello');
+
+
+// * Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+// % a -> 1
+// % e -> 2
+// % i -> 3
+// % o -> 4
+// % u -> 5
+
+// * For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+
+
+// + Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+// + For example, decode("h3 th2r2") would return "hi there".
+// + For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+
+const encode = (word) => {
+    let vowels = [{ ltr: "a", val: "1" }, { ltr: "e", val: "2" }, { ltr: "i", val: "3" }, { ltr: "o", val: "4" }, { ltr: "u", val: "5" }]
+    vowels.forEach((item) => word.includes(item.ltr) ? word = word.replaceAll(item.ltr, item.val) : item)
+    return word
+};
+
+const decode = (word) => {
+    let vowels = [{ ltr: "a", val: "1" }, { ltr: "e", val: "2" }, { ltr: "i", val: "3" }, { ltr: "o", val: "4" }, { ltr: "u", val: "5" }]
+    vowels.forEach((item) => word.includes(item.val) ? word = word.replaceAll(item.val, item.ltr) : item)
+    return word
+};
+
+encode("Suprise, my friend!");
+decode("S5pr3s2, my fr32nd!");
+
+
+
+
