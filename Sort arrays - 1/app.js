259e55651aa4a3ca2630f94e72e6b75ff791a38f
Magma008 <unknownmagma008@gmail.com>
2026-01-22 23:18:08 +0500

    22 January 2026 Thursday 11:18PM

---
+++ Sort arrays - 1/app.js
@@ -0,0 +1,17 @@
+// # assert.deepEqual(sortme(['one', 'two', 'three']), ["one", "three", "two"])
+
+
+// * Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// ^ ["banana", "apple", "cherry"] => ["apple", "banana", "cherry"]
+
+const arraySorter = (array) => {
+    return array.sort();
+};
+
+arraySorter(['one', 'two', 'three']);
+
+
+
+
+
