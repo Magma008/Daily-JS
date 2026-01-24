403be9f6c87e1ea92a7c0fe47db3c261db85d290
Magma008 <unknownmagma008@gmail.com>
2026-01-24 22:38:14 +0500

    24 January 2026 Saturday 10:38PM

--- Nth smallest element/app.js
+++ Nth smallest element/app.js
@@ -10,7 +10,12 @@
 // & Given an array/list of integers, find the Nth smallest element in the array.
 // & Array/list size is at least 3.
 // & Array/list's numbers could be a mixture of positives , negatives and zeros.
-// & Repetition in array/list's numbers could occur, so don't remove duplications.
+// & Repetition in array/list's numbers could occur, so don't remove duplications.'
+
+// $ arr=[3,1,2]            n=2    ==> return 2 
+// $ arr=[15,20,7,10,4,3]   n=3    ==> return 7 
+// $ arr=[2,169,13,-5,0,-1] n=4    ==> return 2 
+// $ arr=[2,1,3,3,1,2],     n=3    ==> return 2 
 
 const smallest = (array, nth) => {
     array.sort((x, y) => x - y);
@@ -18,9 +23,3 @@ const smallest = (array, nth) => {
 };
 
 smallest([2,169,13,-5,0,-1]);
-
-
-
-
-
-
