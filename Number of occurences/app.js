0b828c5b6a7dd7175e49a09fdbd786caf14770f0
Magma008 <unknownmagma008@gmail.com>
2026-01-25 11:54:18 +0500

    25 January 2026 Sunday 11:54AM

---
+++ Number of occurences/app.js
@@ -0,0 +1,28 @@
+// $ assert.strictEqual(arr.slice().numberOfOccurrences(0), 1, `Incorrect answer for 0, arr = [0, 1, 2, 2, 3]`);
+// $ assert.strictEqual(arr.slice().numberOfOccurrences(4), 0, `Incorrect answer for 4, arr = [0, 1, 2, 2, 3]`);
+// $ assert.strictEqual(arr.slice().numberOfOccurrences(2), 2, `Incorrect answer for 2, arr = [0, 1, 2, 2, 3]`);
+// $ assert.strictEqual(arr.slice().numberOfOccurrences(3), 1, `Incorrect answer for 3, arr = [0, 1, 2, 2, 3]`);
+
+
+// + Write a function that returns the number of occurrences of an element in an array.
+// + This function will be defined as a property of Array with the help of the method Object.defineProperty, which allows to define a new method directly on the object.
+
+// ? var arr = [0, 1, 2, 2, 3];
+// ? arr.numberOfOccurrences(0) === 1;
+// ? arr.numberOfOccurrences(4) === 0;
+// ? arr.numberOfOccurrences(2) === 2;
+// ? arr.numberOfOccurrences(3) === 1;
+
+Object.defineProperty(Array.prototype, 'numberOfOccurrences',{ 
+  value : function numberOfOccurrences(element) {
+    return this.filter((item) => item == element).length
+  }
+});
+
+let array = [0, 1, 2, 2, 3, 6, 10];
+array.numberOfOccurrences(2);
+
+
+
+
+
