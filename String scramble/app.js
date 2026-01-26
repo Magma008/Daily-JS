d94775772cab52ccbc739603c20d7c3557d53525
Magma008 <unknownmagma008@gmail.com>
2026-01-26 11:13:32 +0500

    26 January 2026 Monday 11:13AM

---
+++ String scramble/app.js
@@ -0,0 +1,17 @@
+let arr = [0, 3, 1, 2];
+let word = "abcd";
+let nw = [];
+
+arr.map((item, index) => {
+    // console.log(item, index);
+    // nw.push(word.split("").find((i) => i == item))
+    // console.log(word.split("").findIndex(it => it == item));
+    // console.log(word.split("").indexOf());
+    word.split("").map((ltr) => {
+        console.log(ltr);
+    })
+    
+    
+})
+
+// console.log(nw);
