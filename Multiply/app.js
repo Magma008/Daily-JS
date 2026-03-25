f190566dbba82b2c5f525aa70db5ec89d19bccd4
Magma008 <unknownmagma008@gmail.com>
2026-03-25 14:44:45 +0500

    25 March 2026 Wednesday 02:44PM

---
+++ Multiply/app.js
@@ -0,0 +1,18 @@
+// # assert.strictEqual(multiply(1, 1), 1);
+// # assert.strictEqual(multiply(2, 1), 2);
+// # assert.strictEqual(multiply(2, 2), 4);
+// # assert.strictEqual(multiply(3, 5), 15);
+// # assert.strictEqual(multiply(5, 0), 0);
+// # assert.strictEqual(multiply(0, 5), 0);
+// # assert.strictEqual(multiply(0, 0), 0);
+
+// * This code does not execute properly. Try to figure out why.
+
+const mulitply = (a, b) => {
+    return a * b;
+};
+
+mulitply(2, 4);
+
+
+
